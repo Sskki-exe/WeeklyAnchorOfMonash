debe406dcfd119022946538c42c7cab5de909c92
sskki <beaver.hacker@gmail.com>
2023-02-28 23:18:48 +1100

    comments

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from selenium import webdriver
+
+######## TO DO ########
+# For each page:
+#     For each card on page:
+#         Check for dates on the post ("information session: 24th February")
+#         If date is in the past, stop
+#         Else, add post to list
+#
+
 driver = webdriver.Firefox()
 driver.get("https://www.facebook.com/MSA.Freebies/")
